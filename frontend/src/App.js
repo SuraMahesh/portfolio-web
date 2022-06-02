08b522212e5b4fc983ad2ebbac0238fb9036040b
SuraMahesh <surangamunasinghe22568@gmail.com>
2022-06-02 14:36:06 +0530

    complete header section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
 import { About, Skills, Work, Profile, Header, Footer} from './container'
+import { Navbar} from './components'
 
 const App = () => {
   return (
     <div className='app'>
+        <Navbar />
         <Header />
+        <About />
         <Footer />
     </div>
    
---
+++ frontend/src/constants/images.js
@@ -0,0 +1,17 @@
+import profile from '../assets/profile.jpg'
+import flutter from '../assets/flutter.png'
+import redux from '../assets/redux.png'
+import sass from '../assets/sass.png'
+import bgIMG from '../assets/bgIMG.png'
+import bgWhite from '../assets/bgWhite.png'
+import circle from '../assets/circle.svg'
+
+export default {
+    profile,
+    flutter,
+    redux,
+    sass,
+    bgIMG,
+    bgWhite,
+    circle
+}
